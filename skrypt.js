d50105c0feb60de15faf29178874b6a9cb8ea530
Filip12678 <87238077+Filip12678@users.noreply.github.com>
2021-09-15 16:33:03 +0200

    Add files via upload

---
+++ skrypt.js
@@ -0,0 +1,23 @@
+function myFunction1() {
+    window.open("https://filip12678.github.io/stronaDoObliczaniaPola_I_ObwoduKola/");
+  }
+
+  function myFunction2() {
+    window.open("https://filip12678.github.io/stronaDoObliczaniaNWWiNWD/");
+  }
+
+  function myFunction3() {
+    window.open("https://filip12678.github.io/stronaDoObliczania_Poteg_I_Pierwiastkow/");
+  }
+
+  function myFunction4() {
+    window.open("https://filip12678.github.io/Strona_O_LiczbiePI/");
+  }
+
+  function myFunction5() {
+    window.open("https://filip12678.github.io/StronaGenerujacaLiczbyPierwsze/");
+  }
+
+  function myFunction6() {
+    window.open("https://filip12678.github.io/Notatki_Z_Pastebina/");
+  }
